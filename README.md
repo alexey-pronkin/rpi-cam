c2ec8bff4fb17edd82204d3b87205663f8c04ff5
Aleksei Pronkin <31882933+alexey-pronkin@users.noreply.github.com>
2022-12-07 04:59:41 +0300

    refactor, add day/night logic

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # rpi-cam
-Simple raspberry pi camera capture with some handling of captured videos
+Simple raspberry pi camera capture with auto deleting of old captured videos. Also night/day modes in alpha test.
 
+For now, a picamera is hard to install into some virtual environment, so it's better to install all into the system python:
 ```bash
-python -m venv /home/pi/rpi-camv
-source /home/pi/rpi-camv/bin/activate
+sudo apt install -y python3-pyqt5 python3-opengl
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3-picamera2
 pip install ephem
 ```
+
+Users of Raspberry Pi 3 or earlier devices will need to enable *Glamor* in order for this example script using X Windows to work. To do this, run `sudo raspi-config` in a command window, choose Advanced Options and then
+enable *Glamor graphic acceleration*. Finally reboot your device.
 
 Clone repo:
 
@@ -14,13 +20,14 @@ git clone https://github.com/alexey-pronkin/rpi-cam.git
 ```
 
 Change variables inside rpi-cam.py to your values:
-
+```python
 VIDEOS_PATH = '/home/pi/Desktop/videos/'
 MAX_FOLDER_SIZE = 60 * 1_000_000
-VIDEOS_LENGHT = 60 * 60 * 8 # in seconds
+VIDEOS_LENGHT = 60 * 60 * 8 # in seconds, should be divisible by 3600
 USE_NIGHT_MODE = True
 LAT = 55.494120 # Camera latitude
 LON = 38.661637 # Camera longitude
+```
 
 Start recording
 
--- rpi-cam/rpi-cam.py
+++ rpi-cam/rpi-cam.py
@@ -9,11 +9,11 @@ from picamera2.encoders import H264Encoder, Quality
 
 VIDEOS_PATH = '/home/pi/Desktop/videos/'
 MAX_FOLDER_SIZE = 60 * 1_000_000
-VIDEOS_LENGHT = 60 * 60 * 8 # in seconds
+VIDEOS_LENGHT = 60 * 60 * 8 # in seconds, divided by 3600
 USE_NIGHT_MODE = True
 LAT = 55.494120 # Camera latitude
 LON = 38.661637 # Camera longitude
-DEBUG = True
+DEBUG = False
 
 def handle_file(path):
     filenames = os.listdir(path=path)
@@ -29,45 +29,72 @@ def get_size(path):
         folder_size += os.stat(p).st_size
     return folder_size
 
+def get_day_or_night(n):
+    observer = ephem.Observer()
+    sun = ephem.Sun()
+    observer.lat = LAT
+    observer.lon = LON
+    observer.date = n
+    rise_time = observer.next_rising(sun)
+    set_time = observer.next_setting(sun)
+    if (n > set_time.datetime()) and (n < rise_time.datetime()):
+        return "night"
+    else:
+        return "day"
+def night_mode():
+    ## Night mode from tutorial: https://picamera.readthedocs.io/en/release-1.13/fov.html?highlight=night#sensor-gain
+    # Force sensor mode 3 (the long exposure mode), set
+    # the framerate to 5 fps, the shutter speed to 6s,
+    # and ISO to 800 (for maximum gain)
+    camera = Picamera2()
+    if DEBUG > 0:
+        print(camera.sensor_modes)
+    config = camera.create_preview_configuration({'fps': 10})
+    # raw=camera.sensor_modes[2]
+    camera.configure(config)
+    camera.set_controls({"ExposureTime": 250, "AnalogueGain": 8.0})
+    return camera
+
+def day_mode(): 
+    camera = Picamera2()
+    camera.configure(camera.create_video_configuration())
+    return camera
+
+def time_logic(n):
+    if get_day_or_night(n) == "day":
+        camera = day_mode()
+        mode = "day"
+    elif get_day_or_night(n) == "night":
+        camera = night_mode()
+        mode = "night"
+    else:
+        print("BUG, there is only day and night modes")
+    return camera, mode
+
 if __name__ == "__main__":
     os.makedirs(VIDEOS_PATH, exist_ok=True)
+    encoder = H264Encoder()
     while True:
         if get_size(path=VIDEOS_PATH) > MAX_FOLDER_SIZE:
             handle_file(path=VIDEOS_PATH)
         n = datetime.now()
-
         if USE_NIGHT_MODE:
-            observer = ephem.Observer()
-            sun = ephem.Sun()
-            observer.lat = LAT
-            observer.lon = LON
-            observer.date = n
-            rise_time = observer.next_rising(sun)
-            set_time = observer.next_setting(sun)
-            if (n > set_time) and (n < rise_time):
-                ## Night mode from tutorial:
-                # Force sensor mode 3 (the long exposure mode), set
-                # the framerate to 5 fps, the shutter speed to 6s,
-                # and ISO to 800 (for maximum gain)
-                camera = Picamera2()
-                if DEBUG > 0:
-                    print(camera.sensor_modes)
-                config = camera.create_preview_configuration({'fps': 10})
-                # raw=camera.sensor_modes[2]
-                camera.configure(config)
-                camera.set_controls({"ExposureTime": 250, "AnalogueGain": 8.0})
-            else:
-                camera = Picamera2()
+            camera, mode = time_logic(n=n)
         else:
-            camera = Picamera2()
-            camera.configure(camera.create_video_configuration())
-            encoder = H264Encoder()
+            camera = day_mode()
         if DEBUG > 0:
             print(camera.sensor_modes)
         camera.start_preview(Preview.QTGL)
         # camera.annotate_background = picamera.Color('black')
         # camera.annotate_text = n.strftime('%Y-%m-%d %H:%M:%S')
         camera.start_recording(encoder,VIDEOS_PATH+f'{n.isoformat(timespec="seconds")}.h264', Quality.VERY_HIGH)
-        sleep(VIDEOS_LENGHT)
+        for t in range(sleep(VIDEOS_LENGHT) // 3600):
+            n = datetime.now()
+            if USE_NIGHT_MODE:
+                if get_day_or_night(n) != mode:
+                    camera.stop_recording()
+                    camera.stop_preview()
+                    continue
+            sleep(3600)
         camera.stop_recording()
         camera.stop_preview()
